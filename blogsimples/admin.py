2575e6dd1b6cd9f02c5122754d37b481a336e4e4
Augusto Araújo <augustoaraujo4520@gmail.com>
2021-10-07 19:38:43 -0300

    class PostAdmin

--- blogsimples/admin.py
+++ blogsimples/admin.py
@@ -3,4 +3,8 @@ from .models import Post
 # Register your models here.
 
 #Cria uma nova tabela, com isso permite a criação de posts
-admin.site.register(Post)
+@admin.register(Post)
+
+#Classe que cria um mim resumo sobre os posts
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "slug", "author", "created", "update")
